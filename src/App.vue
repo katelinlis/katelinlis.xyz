bac13c663ffe2169eef98276f5adbef92b832018
katelinlis <katelinlis1999@gmail.com>
2020-02-23 02:16:59 +0300

    Version stage 2

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,53 @@
 <template>
   <div id="app">
-    <FooterComponent/>
+
+       
+  <div class="card">
+    <div class="card-body" style="height: 248px">
+                <div class="">
+                     <div  class=" d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white" v-bind:class="{'fixed-top border-bottom shadow-sm':StickyHeader}">
+                <h5 class="my-0 mr-md-auto font-weight-normal">Katelinlis</h5>
+                <nav class="my-2 my-md-0 mr-md-3">
+                <a class="p-2 text-dark" href="#about">About</a>
+                <a class="p-2 text-dark" href="#tech">Tech</a>
+                <a class="p-2 text-dark" href="#social">Social</a>
+                </nav>
+            </div>
+      <p style="padding-left: 1.5rem !important;" class="lead font-italic  my-3">Привет! Меня зовут Кейтлин. <br>
+         Я работаю в сфере веб разработки 5 лет <br>
+         Я могу для вас разработать клиентскую или серверную часть для вашего сайта<br>
+         А так же помоч в доработке той или иной идеи
+         
+         </p>
+    </div>
+   </div>
+        
+      </div>
     <contentPage/>
   </div>
 </template>
 
 <script>
 
-import FooterComponent from '@/components/FooterComponent'
+
 import contentPage from '@/components/content'
 
 export default {
+  data(){
+    return {
+      StickyHeader: false
+    }
+  },
+  created() {
+    
+    window.addEventListener('scroll', ()=> {
+      console.log( pageYOffset);
+      if(pageYOffset >= 280) this.StickyHeader = true
+      else this.StickyHeader = false
+    });
+  },
   components: {
-    FooterComponent,
+
     contentPage
   },
   name: 'App',
@@ -22,7 +57,7 @@ export default {
 <style>
 
   .container {
-    padding-top: 65px;
+    padding-top: 30px;
   }
   .mainCard{
     background: rgba(237,237,237,1);
--- src/components/content.vue
+++ src/components/content.vue
@@ -1,44 +1,98 @@
 <template>
     <div class="container">
-      <div class="jumbotron p-4 p-md-5  rounded mainCard">
-        <div class="col-md-6 px-0">
-          <h1 class="">Привет? Я Кейт</h1>
-          <p class="lead my-3">Я фронтенд разработчик, использую перспективные технологии для разработки различных веб приложений</p>
-        </div>
-      </div>
-      <div class="card MyTechs">
-        <div id="tech" class="card-body ">
-          Я фронтенд разработчик, использую перспективные технологии для разработки различных веб приложений
-          <br>
-          <br>
-          Frontend:
-          <ul>
-            <li>JavaScript</li>
-            <li>VueJS</li>
-            <li>NuxtJS</li>
-            <li>GraphQL</li>
-            <li>Apollo</li>
-            <li>Bootstrap</li>
-          </ul>
-          Backend: 
-          <ul>
-            <li>Golang</li>
-            <li>Gorilla</li>
-            <li>PostgreSQL</li>
-          </ul>
-          
-          Other: GitHub, GitLab, Selectl
-        </div>
-      </div>
 
-      <div id="social" class="card MyTechs">
-        <div class="card-body ">
-          Мои соц сети
-          <ul>
-            <li><a href="https://vk.com/katelinlis">Vkontakte</a></li>
-            <li><a href="https://github.com/katelinlis">GitHub</a></li>
-          </ul>
-        </div>
+
+
+      <div class="container"> 
+         <div style="text-align: center"> <h4>My Skills </h4> </div>
+
+
+            <div class="row">
+                <div class="col-sm">
+                    <div style="text-align: center">
+                    <img src="@/assets/frontend.png" alt="..." class="rounded-circle img75">
+                    </div>
+                    <div class="card">
+                        
+                        <div id="tech" class="card-body ">
+                            <ul>
+                                <li>HTML/CSS/JavaScript/Bootstrap</li>
+                                <li>VueJS/NuxtJS</li>
+                                <li>GraphQL/Apollo</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm">
+                    <div style="text-align: center">
+                    <img src="@/assets/database.png" alt="..." class="rounded-circle img75">
+                    </div>
+                    <div class="card">
+                        
+                        <div id="tech" class="card-body ">
+                    
+                            <ul>
+                                <li>Golang</li>
+                                <li>Gorilla</li>
+                                <li>PostgreSQL</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm">
+                    <div style="text-align: center">
+                    <img src="@/assets/buffer.png" alt="..." class="rounded-circle img75">
+                    </div>
+                    <div class="card">
+                        
+                        <div id="tech" class="card-body ">
+                    
+                            <ul>
+                                <li>GitHub</li>
+                                <li>GitLab</li>
+                                <li>Selectl</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+        
+      </div>
+<div class="container" style="margin-top: 60px;"> 
+    <div style="text-align: center"> <h4>Мои контакты </h4> </div>
+      <div class="row" style="text-align: center;">
+
+      <div class="col-lg-4">
+          <a href="https://vk.com/katelinlis" style="color: #000 !important; text-decoration: none;">
+            
+            <svg xmlns="http://www.w3.org/2000/svg" width="90" height="90" viewBox="32 32 960 960"><path d="M179.928 32h664.144C925.767 32 992 98.23 992 179.929v664.143C992 925.768 925.767 992 844.072 992H179.928C98.23 992 32 925.768 32 844.071V179.929C32 98.23 98.23 32 179.928 32" fill="#4c75a3"/><path d="M503.946 704.029h39.269s11.859-1.307 17.923-7.831c5.573-5.996 5.396-17.249 5.396-17.249s-.769-52.692 23.683-60.452c24.113-7.647 55.07 50.925 87.88 73.449 24.812 17.039 43.667 13.31 43.667 13.31l87.739-1.227s45.895-2.832 24.132-38.917c-1.782-2.947-12.679-26.693-65.238-75.48-55.02-51.062-47.644-42.801 18.626-131.128 40.358-53.79 56.49-86.628 51.449-100.691-4.803-13.399-34.489-9.86-34.489-9.86l-98.786.611s-7.328-.998-12.756 2.25c-5.31 3.177-8.717 10.599-8.717 10.599s-15.642 41.622-36.486 77.024c-43.988 74.693-61.58 78.647-68.771 74.002-16.729-10.811-12.549-43.422-12.549-66.596 0-72.389 10.98-102.57-21.381-110.384-10.737-2.591-18.646-4.305-46.11-4.585-35.25-.358-65.078.108-81.971 8.384-11.239 5.504-19.91 17.766-14.626 18.472 6.53.869 21.313 3.99 29.152 14.655 10.126 13.776 9.772 44.703 9.772 44.703s5.818 85.213-13.585 95.795c-13.314 7.26-31.582-7.561-70.8-75.327-20.091-34.712-35.265-73.085-35.265-73.085s-2.922-7.17-8.141-11.008c-6.33-4.65-15.175-6.124-15.175-6.124l-93.876.613s-14.09.394-19.267 6.521c-4.606 5.455-.368 16.725-.368 16.725s73.49 171.942 156.711 258.592c76.315 79.454 162.958 74.239 162.958 74.239" fill="#fff"/></svg>
+            <h2>VK</h2>
+          </a>
+      </div>
+      <div class="col-lg-4">
+          <a href="https://github.com/katelinlis" style="color: #000 !important; text-decoration: none;">
+          <svg width="90" height="90" version="1.2" baseProfile="tiny" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 2350 2314.8" xml:space="preserve"><path d="M1175,0C525.8,0,0,525.8,0,1175c0,552.2,378.9,1010.5,890.1,1139.7c-5.9-14.7-8.8-35.3-8.8-55.8v-199.8H734.4c-79.3,0-152.8-35.2-185.1-99.9c-38.2-70.5-44.1-179.2-141-246.8c-29.4-23.5-5.9-47,26.4-44.1c61.7,17.6,111.6,58.8,158.6,120.4c47,61.7,67.6,76.4,155.7,76.4c41.1,0,105.7-2.9,164.5-11.8c32.3-82.3,88.1-155.7,155.7-190.9c-393.6-47-581.6-240.9-581.6-505.3c0-114.6,49.9-223.3,132.2-317.3c-26.4-91.1-61.7-279.1,11.8-352.5c176.3,0,282,114.6,308.4,143.9c88.1-29.4,185.1-47,284.9-47c102.8,0,196.8,17.6,284.9,47c26.4-29.4,132.2-143.9,308.4-143.9c70.5,70.5,38.2,261.4,8.8,352.5c82.3,91.1,129.3,202.7,129.3,317.3c0,264.4-185.1,458.3-575.7,499.4c108.7,55.8,185.1,214.4,185.1,331.9V2256c0,8.8-2.9,17.6-2.9,26.4C2021,2123.8,2350,1689.1,2350,1175C2350,525.8,1824.2,0,1175,0L1175,0z"/></svg>
+
+        <h2>GitHub</h2>
+          </a>
+      </div>
+      <div class="col-lg-4">
+          <a href="https://www.patreon.com/katelinlis" style="color: #000 !important; text-decoration: none;">
+        <svg  viewBox="0 0 567.18 545.8" xmlns="http://www.w3.org/2000/svg" width="90" height="90"><circle cx="362.59" cy="204.59" fill="#e85b46" r="204.59"/><path d="M0 0h100v545.8H0z" fill="#241e12"/></svg>
+        <h2>Patreon</h2>
+          </a>
       </div>
     </div>
+</div>
+
+
+    </div>
 </template>
+
+<style scoped>
+.img75{
+    width: 75px;
+    height: 75px;
+}
+</style>
