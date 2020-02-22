d257a3db54fefe50f20fc5efda1a8895e259335f
katelinlis <katelinlis1999@gmail.com>
2020-02-22 05:58:26 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,49 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <FooterComponent/>
+    <contentPage/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import FooterComponent from '@/components/FooterComponent'
+import contentPage from '@/components/content'
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
+    FooterComponent,
+    contentPage
+  },
+  name: 'App',
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+  .container {
+    padding-top: 65px;
+  }
+  .mainCard{
+    background: rgba(237,237,237,1);
+    background: -moz-linear-gradient(45deg, rgba(237,237,237,1) 0%, rgba(246,246,246,1) 47%, rgba(237,237,237,1) 100%);
+    background: -webkit-gradient(left bottom, right top, color-stop(0%, rgba(237,237,237,1)), color-stop(47%, rgba(246,246,246,1)), color-stop(100%, rgba(237,237,237,1)));
+    background: -webkit-linear-gradient(45deg, rgba(237,237,237,1) 0%, rgba(246,246,246,1) 47%, rgba(237,237,237,1) 100%);
+    background: -o-linear-gradient(45deg, rgba(237,237,237,1) 0%, rgba(246,246,246,1) 47%, rgba(237,237,237,1) 100%);
+    background: -ms-linear-gradient(45deg, rgba(237,237,237,1) 0%, rgba(246,246,246,1) 47%, rgba(237,237,237,1) 100%);
+    background: linear-gradient(45deg, rgba(237,237,237,1) 0%, rgba(246,246,246,1) 47%, rgba(237,237,237,1) 100%);
+    filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#ededed', endColorstr='#ededed', GradientType=1 );
+  }
+
+  .MyTechs{
+    margin-bottom: 30px;
+    background: rgba(242,234,242,1);
+    background: -moz-linear-gradient(45deg, rgba(242,234,242,1) 0%, rgba(255,232,255,1) 100%);
+    background: -webkit-gradient(left bottom, right top, color-stop(0%, rgba(242,234,242,1)), color-stop(100%, rgba(255,232,255,1)));
+    background: -webkit-linear-gradient(45deg, rgba(242,234,242,1) 0%, rgba(255,232,255,1) 100%);
+    background: -o-linear-gradient(45deg, rgba(242,234,242,1) 0%, rgba(255,232,255,1) 100%);
+    background: -ms-linear-gradient(45deg, rgba(242,234,242,1) 0%, rgba(255,232,255,1) 100%);
+    background: linear-gradient(45deg, rgba(242,234,242,1) 0%, rgba(255,232,255,1) 100%);
+    filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#f2eaf2', endColorstr='#ffe8ff', GradientType=1 );
+  }
 </style>
---
+++ src/components/content.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="container">
+      <div class="jumbotron p-4 p-md-5  rounded mainCard">
+        <div class="col-md-6 px-0">
+          <h1 class="">Привет? Я Кейт</h1>
+          <p class="lead my-3">Я фронтенд разработчик, использую перспективные технологии для разработки различных веб приложений</p>
+        </div>
+      </div>
+      <div class="card MyTechs">
+        <div id="tech" class="card-body ">
+          Я фронтенд разработчик, использую перспективные технологии для разработки различных веб приложений
+          <br>
+          <br>
+          Frontend:
+          <ul>
+            <li>JavaScript</li>
+            <li>VueJS</li>
+            <li>NuxtJS</li>
+            <li>GraphQL</li>
+            <li>Apollo</li>
+            <li>Bootstrap</li>
+          </ul>
+          Backend: 
+          <ul>
+            <li>Golang</li>
+            <li>Gorilla</li>
+            <li>PostgreSQL</li>
+          </ul>
+          
+          Other: GitHub, GitLab, Selectl
+        </div>
+      </div>
+
+      <div id="social" class="card MyTechs">
+        <div class="card-body ">
+          Мои соц сети
+          <ul>
+            <li><a href="https://vk.com/katelinlis">Vkontakte</a></li>
+            <li><a href="https://github.com/katelinlis">GitHub</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+</template>
